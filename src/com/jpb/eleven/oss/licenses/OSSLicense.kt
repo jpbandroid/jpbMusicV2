f11d30726eb6b070e0de11dbefa115257d7cb539
jpb <jakub.baranski.33@gmail.com>
2022-03-13 18:39:13 +0000

    Add OSS Licenses

--- src/com/jpb/eleven/oss/licenses/OSSLicense.kt
+++ src/com/jpb/eleven/oss/licenses/OSSLicense.kt
@@ -3,14 +3,24 @@ package com.jpb.eleven.oss.licenses
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.insets.LocalWindowInsets
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.rememberInsetsPaddingValues
+import com.google.accompanist.insets.ui.Scaffold
+import com.google.accompanist.insets.ui.TopAppBar
 import com.jpb.eleven.oss.licenses.ui.theme.Elevenlineage181Theme
+import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
 
 class OSSLicense : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +32,7 @@ class OSSLicense : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainLayout()
                 }
             }
         }
@@ -30,14 +40,49 @@ class OSSLicense : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MainLayout() {
+    MaterialTheme(
+        colors = if (isSystemInDarkTheme()) darkColors() else lightColors()
+    ) {
+        ProvideWindowInsets {
+            var showAuthor by remember { mutableStateOf(true) }
+            var showVersion by remember { mutableStateOf(true) }
+            var showLicenseBadges by remember { mutableStateOf(true) }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Elevenlineage181Theme {
-        Greeting("Android")
+            Scaffold(
+                topBar = {
+                    // We use TopAppBar from accompanist-insets-ui which allows us to provide
+                    // content padding matching the system bars insets.
+                    TopAppBar(
+                        title = { Text("Open Source Licenses") },
+                        backgroundColor = MaterialTheme.colors.surface,
+                        contentPadding = rememberInsetsPaddingValues(
+                            LocalWindowInsets.current.statusBars,
+                            applyBottom = false,
+                        ),
+                        modifier = Modifier.fillMaxWidth(),
+                        actions = {
+                            IconButton(onClick = { showAuthor = !showAuthor }) { Icon(Icons.Default.Person, "Author") }
+                            IconButton(onClick = { showVersion = !showVersion }) { Icon(Icons.Default.Build, "Version") }
+                            IconButton(onClick = { showLicenseBadges = !showLicenseBadges }) { Icon(
+                                Icons.Default.List, "Licenses") }
+                        }
+                    )
+                },
+            ) { contentPadding ->
+                LibrariesContainer(
+                    Modifier.fillMaxSize(),
+                    contentPadding = rememberInsetsPaddingValues(
+                        insets = LocalWindowInsets.current.systemBars,
+                        additionalTop = contentPadding.calculateTopPadding(),
+                        applyTop = false,
+                        applyBottom = true
+                    ),
+                    showAuthor = showAuthor,
+                    showVersion = showVersion,
+                    showLicenseBadges = showLicenseBadges
+                )
+            }
+        }
     }
 }
